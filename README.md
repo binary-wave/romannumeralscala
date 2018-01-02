e13b13d3afff79fc76ad5fdd47d577b1a3c2ba9f
binary-wave <crackerjack.genius@gmail.com>
2018-01-02 21:40:54 +0900

    Initial source commit.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# romannumeralscala
-scala practice
+RomanNumeralScala
+===================
+
+Implementation of the Roman numerals in Scala.
+
+# Usage
+```scala
+import org.binwave.RomanNumeral
+val n = RomanNumeral(3)
+println(n.toInt + " is " + n + " in Roman numerals.")
+```
---
+++ src/main/scala/org/binwave/RomanNumeral.scala
@@ -0,0 +1,108 @@
+package org.binwave
+
+/**
+  * ローマ数字を表すクラス
+  *
+  * @param intValue 数値
+  */
+class RomanNumeral(intValue: Int) {
+  // ローマ数字は1から3999までしか表すことができない
+  require(intValue > 0 && intValue < 4000, s"intValue must be between 1 and 3999. : $intValue")
+
+  /**
+    * 数値
+    */
+  private val value: Int = intValue
+
+  /**
+    * Int型の値を返す
+    *
+    * @return 数値
+    */
+  def toInt: Int = value
+
+  /**
+    * 文字列に変換する
+    *
+    * @return ローマ数字をアルファベット大文字で表した文字列
+    */
+  override def toString: String = roman(THOUSAND) + roman(HUNDRED) + roman(TEN) + roman(ONE)
+
+  /**
+    * 指定された桁のローマ数字を計算する
+    *
+    * @param ch 対象とする桁の文字セット
+    * @return
+    */
+  private def roman(ch: RomanChars): String = {
+    value / ch.unit % 10 match {
+      case 9 => ch.one + ch.ten
+      case 4 => ch.one + ch.five
+      case n: Int => ch.five * (n / 5) + ch.one * (n % 5)
+    }
+  }
+
+  /**
+    * 1桁のローマ数字を表す文字セット
+    *
+    * @param unit 単位（oneに相当する数値）
+    * @param one  1に対応する文字
+    * @param five 5に対応する文字
+    * @param ten  10に対応する文字
+    */
+  private abstract case class RomanChars(unit: Int, one: String, five: String, ten: String)
+
+  /**
+    * 1000の単位を表す文字セット
+    */
+  private object THOUSAND extends RomanChars(1000, "M", null, null)
+
+  /**
+    * 100の単位を表す文字セット
+    */
+  private object HUNDRED extends RomanChars(100, "C", "D", "M")
+
+  /**
+    * 10の単位を表す文字セット
+    */
+  private object TEN extends RomanChars(10, "X", "L", "C")
+
+  /**
+    * 1の単位を表す文字セット
+    */
+  private object ONE extends RomanChars(1, "I", "V", "X")
+
+  def +(other: RomanNumeral): RomanNumeral = RomanNumeral(value + other.value)
+
+  def -(other: RomanNumeral): RomanNumeral = RomanNumeral(value - other.value)
+
+  def *(other: RomanNumeral): RomanNumeral = RomanNumeral(value * other.value)
+
+  def /(other: RomanNumeral): RomanNumeral = RomanNumeral(value / other.value)
+
+  def ==(other: RomanNumeral): Boolean = value == other.value
+
+  def >(other: RomanNumeral): Boolean = value > other.value
+
+  def >=(other: RomanNumeral): Boolean = value >= other.value
+
+  def <(other: RomanNumeral): Boolean = value < other.value
+
+  def <=(other: RomanNumeral): Boolean = value <= other.value
+}
+
+object RomanNumeral {
+  /**
+    * ローマ数字で表すことのできる最大値
+    */
+  val MIN = RomanNumeral(1)
+
+  /**
+    * ローマ数字で表すことのできる最小値
+    */
+  val MAX = RomanNumeral(3999)
+
+  def apply(int: Int): RomanNumeral = new RomanNumeral(int)
+
+  def unapply(arg: RomanNumeral): Option[Int] = Some(arg.value)
+}
